3526b0174f21fead3ba75450be42e6ba3b97f013
tiradopablo <pablo.tirado@xepelin.com>
2022-08-25 23:46:10 -0300

    delete build

--- build/bot/auth/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isAllowUser = void 0;
-const users_1 = require("../../entities/users");
-const isAllowUser = (msg) => users_1.allowedUsers.includes(msg.from);
-exports.isAllowUser = isAllowUser;
